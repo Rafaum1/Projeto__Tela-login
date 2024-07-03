1f6c60a973cd2d937006b931c4c5e87510344ce9
Rafael Miranda <134517210+Rafaum1@users.noreply.github.com>
2024-07-03 00:13:09 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function createAccount() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("create").style.display = "flex";
+}
+
+function loginAccount() {
+    document.getElementById("login").style.display = "flex";
+    document.getElementById("create").style.display = "none";
+}
